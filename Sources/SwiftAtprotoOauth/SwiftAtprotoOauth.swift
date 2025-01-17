d7799da0523a9389c669d0179e91e658dcc5cdf7
XfableX <lachlan.maxwell@carey.com.au>
2025-01-18 00:21:50 +1100

    Update SwiftAtprotoOauth.swift

--- Sources/SwiftAtprotoOauth/SwiftAtprotoOauth.swift
+++ Sources/SwiftAtprotoOauth/SwiftAtprotoOauth.swift
@@ -22,7 +22,7 @@ import dnssd
 import OAuth2
 
 @available(macOS 12.0, *)
-class SwiftAtprotoOauth {
+open class SwiftAtprotoOauth {
     
     public func resolveEndpoint(handle: String) async throws -> URL {
         do{
